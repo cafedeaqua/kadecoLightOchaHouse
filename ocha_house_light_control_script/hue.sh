85262476a472ebf24241079119b844f37d730e1b
cafedeaqua <cafedeaqua@users.noreply.github.com>
2014-02-25 00:46:18 +0900

    Add to control ocha house light from bash script

---
+++ ocha_house_light_control_script/hue.sh
@@ -0,0 +1,39 @@
+﻿#!/bin/bash
+rm -f call*
+
+change_contrast(){
+# 100%
+wget "http://192.168.1.142:31413/call.json?method=set&params=[GeneralLighting,[0xB0,0x64]]"
+# 050%
+#wget "http://192.168.1.142:31413/call.json?method=set&params=[GeneralLighting,[0xB0,0x32]]"
+# 000%
+wget "http://192.168.1.142:31413/call.json?method=set&params=[GeneralLighting,[0xB0,0x00]]"
+return 0	
+}
+
+for ((i=0;i<1000;++i))
+do
+#ON
+wget "http://192.168.1.142:31413/call.json?method=set&params=[GeneralLighting,[0x80,0x30]]"
+# 100%
+wget "http://192.168.1.142:31413/call.json?method=set&params=[GeneralLighting,[0xB0,0x64]]"
+# 0x42 white
+wget "http://192.168.1.142:31413/call.json?method=set&params=[GeneralLighting,[0xB6,0x42]]"
+change_contrast
+
+#0x43 red
+wget "http://192.168.1.142:31413/call.json?method=set&params=[GeneralLighting,[0xB6,0x43]]"
+change_contrast
+
+#0x41 murasaki 
+wget "http://192.168.1.142:31413/call.json?method=set&params=[GeneralLighting,[0xB6,0x41]]"
+change_contrast
+
+#0x45 yellow
+wget "http://192.168.1.142:31413/call.json?method=set&params=[GeneralLighting,[0xB6,0x45]]"
+change_contrast
+
+#OFF
+wget "http://192.168.1.142:31413/call.json?method=set&params=[GeneralLighting,[0x80,0x31]]"
+
+done
---
+++ ocha_house_light_control_script/otya_light.sh
@@ -0,0 +1,23 @@
+﻿#!/bin/bash
+rm -f call*
+for ((i=0;i<2;++i))
+do
+wget "http://192.168.1.142:31413/call.json?method=set&params=[GeneralLighting1,[0x80,0x30]]"
+sleep 1000
+wget "http://192.168.1.142:31413/call.json?method=set&params=[GeneralLighting2,[0x80,0x30]]"
+sleep 1000 
+wget "http://192.168.1.142:31413/call.json?method=set&params=[GeneralLighting3,[0x80,0x30]]"
+sleep 1000
+wget "http://192.168.1.142:31413/call.json?method=set&params=[GeneralLighting4,[0x80,0x30]]"
+sleep 1000
+wget "http://192.168.1.142:31413/call.json?method=set&params=[GeneralLighting1,[0x80,0x31]]"
+sleep 1000
+wget "http://192.168.1.142:31413/call.json?method=set&params=[GeneralLighting2,[0x80,0x31]]"
+sleep 1000
+wget "http://192.168.1.142:31413/call.json?method=set&params=[GeneralLighting3,[0x80,0x31]]"
+sleep 1000
+wget "http://192.168.1.142:31413/call.json?method=set&params=[GeneralLighting4,[0x80,0x31]]"
+sleep 1000
+done
+
+
